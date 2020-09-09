974ec501e2725d75a1c4adde02a29f5d67b920dc
Khushi-Verma <44372851+Khushi-Verma@users.noreply.github.com>
2020-09-09 18:37:04 +0530

    Updates

---
+++ precache-manifest.fb1e7a77913c5b25cad02669b835616e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539bbc8c896e642fa62b179334e32d88",
+    "url": "/NetflixApp/index.html"
+  },
+  {
+    "revision": "ea891f4a414b32866bcd",
+    "url": "/NetflixApp/static/css/main.dbbd7c28.chunk.css"
+  },
+  {
+    "revision": "08983269fcff05146168",
+    "url": "/NetflixApp/static/js/2.95d03d3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NetflixApp/static/js/2.95d03d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea891f4a414b32866bcd",
+    "url": "/NetflixApp/static/js/main.cad03adf.chunk.js"
+  },
+  {
+    "revision": "ec781238037e74aa0364",
+    "url": "/NetflixApp/static/js/runtime-main.41f7177b.js"
+  }
+]);
